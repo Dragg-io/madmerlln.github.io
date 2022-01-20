e9b0ab9acc6498e12d84c021587f2c3e4203d46c
Lucas Silva <lucas.silva.ext@tatix.com.br>
2022-01-20 01:13:07 -0300

    Módulo middleware finalizado

--- middleware/script.js
+++ middleware/script.js
@@ -1,38 +1,14 @@
-function reducer(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENTAR':
-      return state + 1;
-    case 'REDUZIR':
-      return state - 1;
-    default:
-      return state;
+import store from './store/storeConfigure.js';
+import { tokenFetch } from './store/modules/token.js';
+import { userFetch } from './store/modules/user.js';
+
+const login = async (user) => {
+  let state = store.getState();
+  if (state.token.data === null) {
+    await store.dispatch(tokenFetch(user));
   }
-}
-
-const logger = (store) => (next) => (action) => {
-  console.group(action.type);
-  console.log('ACTION', action);
-  console.log('PREV_STATE', store.getState());
-  const result = next(action);
-  console.log('NEW_STATE', store.getState());
-  console.groupEnd();
-  return result;
+  state = store.getState();
+  await store.dispatch(userFetch(state.token.data));
 };
 
-const reduzirMiddle = (store) => (next) => (action) => {
-  if (action.type === 'REDUZIR') window.alert('REDUZIU');
-  return next(action);
-};
-
-const { applyMiddleware, compose } = Redux;
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const enhancer = composeEnhancers(Redux.applyMiddleware(logger, reduzirMiddle));
-
-const store = Redux.createStore(reducer, enhancer);
-
-console.log(store.getState());
-
-store.dispatch({ type: 'INCREMENTAR' });
-store.dispatch({ type: 'REDUZIR' });
+login({ username: 'dog', password: 'dog' });
